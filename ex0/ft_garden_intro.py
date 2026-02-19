cd3ab80b4ce4f2cb6091f42eb76d7e47ac7f1605
aboussab <aminebossaboune@gmail.com>
2026-02-19 17:22:32 +0000

    1st

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__  == "__main__":
+    name = "rose"
+    hight = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print("Plant:", name)
+    print("Height: ", hight,"cm", sep ="")
+    print("Age:", hight,"days", sep =" ", end = "\n\n")
+    print("=== End of Program ===")
