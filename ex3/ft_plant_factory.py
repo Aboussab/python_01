e014ca635fe28588106b1cc6ca00a46ca3485cf0
amine-XB8999999999999 <aminebossaboune@gmail.com>
2026-02-23 16:35:30 +0000

    ex4

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -2,14 +2,21 @@ class Plant:
     def __init__(self, name : str, height : int, age : int) -> None :
         self.name = name
         self.height = height
-        self.age_cur = age
+        self.age = age
 
+
+plants = [
+    ["Rose", 25, 30],
+    ["Oak", 200, 365],
+    ["Cactus", 5, 90],
+    ["Sunflower", 80, 45],
+    ["Fern", 15, 120]]
+j = 0
+for x in plants :
+        plants[j] = Plant(x[0],x[1],x[2])
+        j = j + 1
 if __name__ == "__main__":
-    plants = [
-        ["Rose", 25, 30],
-        ["Oak", 200, 365],
-        ["Cactus", 5, 90],
-        ["Sunflower", 80, 45],
-        ["Fern", 15, 120]]
+    print("=== Plant Factory Output ===")
     for x in plants:
-        
+        print(f"Created : {x.name} ({x.height}cm, {x.age} days)")
+    print(f"\nTotal plants created:",j)
